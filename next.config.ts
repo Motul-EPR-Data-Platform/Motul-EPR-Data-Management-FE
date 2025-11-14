8f55c38a9125dbdb6ff906c8b8bafc8369c98919
Kat <91169645+Pyxbi@users.noreply.github.com>
2025-11-14 15:25:34 +0700

    format code

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
-   output: 'export',
-}
+  output: "export",
+};
 
-export default nextConfig
+export default nextConfig;
